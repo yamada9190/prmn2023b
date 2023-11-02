4e9bbda52d88c871a6fa44c03cb3a933700a7230
yamada9190 <kyousuke.20039190@au.com>
2023-11-02 12:31:03 +0900

    class作成

---
+++ prmn2023b/src/main/java/lecture01/Main.java
@@ -0,0 +1,11 @@
+package lecture01;
+
+public class Main {
+    public static void main(String[] args) {
+        //本来ならここに処理を書く
+        //スラッシュを2つ打つと、コメント文として認識される。
+        //コードの可読性の向上につながるので、書く癖をつけよう
+        //行を選択して「ctrl+/ (command+/)」で選択行をコメント文に変更できる(複数行可)
+    }
+
+}
